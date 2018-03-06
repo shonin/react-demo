30884812f41a82b07b5edb7a40f48c0c2600f9a0
Elliot Charney <elliotcharney@gmail.com>
2018-03-06 08:10:40 -0800

    have multiple keys that say their coloir when clicked

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ import Display from './Display.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {beingClicked: false};
+    this.state = {beingClicked: "none"};
 
     this.beingClicked = this.beingClicked.bind(this);
+    this.notBeingClicked = this.notBeingClicked.bind(this);
   }
 
-  beingClicked() {
-    this.setState({beingClicked: !this.state.beingClicked})
+  beingClicked(color) {
+    this.setState({beingClicked: color})
+  }
+
+  notBeingClicked() {
+    this.setState({beingClicked: "none"})
   }
 
   render() {
     return (
       <div>
-        <Key changeDisplay={this.beingClicked}/>
-        <Display displayText={this.state.beingClicked ? "clicked" : "not clicked"}/>
+        <Key changeDisplay={this.beingClicked} resetDisplay={this.notBeingClicked} color="red"/>
+        <Key changeDisplay={this.beingClicked} resetDisplay={this.notBeingClicked} color="orange"/>
+        <Key changeDisplay={this.beingClicked} resetDisplay={this.notBeingClicked} color="yellow"/>
+        <Key changeDisplay={this.beingClicked} resetDisplay={this.notBeingClicked} color="green"/>
+        <Key changeDisplay={this.beingClicked} resetDisplay={this.notBeingClicked} color="blue"/>
+        <Key changeDisplay={this.beingClicked} resetDisplay={this.notBeingClicked} color="indigo"/>
+        <Key changeDisplay={this.beingClicked} resetDisplay={this.notBeingClicked} color="violet"/>
+        <Display displayText={this.state.beingClicked}/>
       </div>
     );
   }
--- src/Key.js
+++ src/Key.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 class Key extends Component {
+  constructor(props) {
+    super(props);
+    this.triggerDisplayChange = this.triggerDisplayChange.bind(this);
+  }
+
+  triggerDisplayChange() {
+    this.props.changeDisplay(this.props.color)
+  }
+
   render() {
     return(
       <div
-        onMouseDown={this.props.changeDisplay}
-        onMouseUp={this.props.changeDisplay}
-        style={{width: '200px', height: '200px', backgroundColor: 'red'}}
+        onMouseDown={this.triggerDisplayChange}
+        onMouseUp={this.props.resetDisplay}
+        style={{
+          display: 'inline-block',
+          width: '50px',
+          height: '200px',
+          backgroundColor: this.props.color}}
       />
     );
   }
